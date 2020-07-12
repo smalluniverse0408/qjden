574647a359641b706256e017aac2eeff15408011
mengyuj <yujing.meng@capgemini.com>
2020-07-13 02:58:45 +0800

    master分支存储build后的代码

---
+++ precache-manifest.7187435412744924c6c3f247f5707d81.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8bc74c98381f53d48a",
+    "url": "css/app.1078b8e1.css"
+  },
+  {
+    "revision": "21439be5ca5a945afe96",
+    "url": "css/chunk-vendors.9a0da246.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "5b632fbdff9ea2550be29ae6342cb8c7",
+    "url": "index.html"
+  },
+  {
+    "revision": "eb8bc74c98381f53d48a",
+    "url": "js/app.4f345674.js"
+  },
+  {
+    "revision": "21439be5ca5a945afe96",
+    "url": "js/chunk-vendors.301c68a6.js"
+  },
+  {
+    "revision": "95a8c32db95be586a92233fb5bdbafe0",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
